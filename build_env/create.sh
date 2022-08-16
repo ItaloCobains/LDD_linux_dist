2b2acaa28091b366b4c86ae604ded3debdfc8e2e
ItaloCobains <monokatchaleger@gmail.com>
2022-08-16 17:18:39 -0300

    script ambiente

--- build_env/create.sh
+++ build_env/create.sh
@@ -1,9 +1,18 @@
+set -e 
+
 echo "Dist Root: ${DIST_ROOT:?}"
 echo "LFS: ${LFS:?}"
 
-if ! test $(whoami) == "distbuild" ; then
-    echo "Must run as distbuild!"
-    exit -1
-fi
+# if ! test $(whoami) == "distbuild" ; then
+#     echo "Must run as distbuild!"
+#     exit -1
+# fi
 
 echo "Crete build environment...."
+
+cd $LFS/sources
+
+cd $DIST_ROOT/build_env
+
+bash -e build_scripts/binutils-pass-1.sh 
+bash -e build_scripts/gcc-pass-1.sh
