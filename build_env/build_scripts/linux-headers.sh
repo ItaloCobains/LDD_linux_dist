ec3a3177de9a821193b15e0a81d7c012bbd30be4
ItaloCobains <monokatchaleger@gmail.com>
2022-08-16 22:22:24 -0300

    more tools

---
+++ build_env/build_scripts/linux-headers.sh
@@ -0,0 +1,21 @@
+echo 
+echo "LINUX HEADER"
+echo 
+sleep 1
+
+cd $LFS/sources
+tar -xf linux-5.10.17.tar.xz
+cd linux-5.10.17
+
+make mrproper
+
+make headers
+find usr/include -name '.*' -delete
+rm usr/include/Makefile
+cp -rv usr/include $LFS/usr
+
+
+
+cd $LFS/sources
+
+rm -rf linux-5.10.17
--- build_env/create.sh
+++ build_env/create.sh
@@ -14,5 +14,10 @@ cd $LFS/sources
 
 cd $DIST_ROOT/build_env
 
-bash -e build_scripts/binutils-pass-1.sh 
-bash -e build_scripts/gcc-pass-1.sh
+# bash -e build_scripts/binutils-pass-1.sh 
+# bash -e build_scripts/gcc-pass-1.sh
+# bash -e build_scripts/linux-headers.sh
+# bash -e build_scripts/glibc.sh
+
+
+echo "Done"
